ba7b08691dfa2e940ef7ea171355124df4aefb46
DavidAshburn <oneinchwebbing@gmail.com>
2022-11-01 12:03:24 -0700

    styles for show card/course and handled some edge cases in course search styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   #trim these down later
-  resources :discs, :courses, :disckeys, :coursekeys
+  resources :discs, :courses, :disckeys, :coursekeys, :cards
   devise_for :users
   root 'home#index'
   get 'home/discbag'
+  get 'home/courses'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
